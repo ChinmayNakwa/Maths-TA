b5969916b4d867cd1b38c4d17cb878ee17cf663d
ChinmayNakwa <chinmaynakwa29012005@gmail.com>
2025-10-29 20:26:17 +0530

    fix: simplify Vercel config and update CORS

--- api/app.py
+++ api/app.py
@@ -23,6 +23,7 @@ origins = [
     "http://localhost:8000", 
     "http://127.0.0.1:3000", # <-- ADD THIS for your HTML/JS frontend
     "http://localhost:3000",  
+    "*"
     # Add your deployed frontend URL here
     # e.g., "https://maths-ta-frontend.onrender.com" 
 ]
@@ -30,7 +31,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
